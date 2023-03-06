8fa2793b391cad2004d5b8cc073938ba6151c40b
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2023-03-06 15:49:23 +0530

    database

--- store_bcpl.sql
+++ store_bcpl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2022 at 11:47 AM
+-- Generation Time: Mar 02, 2023 at 07:49 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.29
 
